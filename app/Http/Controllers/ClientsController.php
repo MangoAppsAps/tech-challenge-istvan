a346f5fa4e46854143c21bfb3114fec86011ee5f
Istvan Palinkas <pistvan@pistvan.hu>
2024-08-30 16:32:57 +0200

    feature: add validation for client creation

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Client;
+use App\Rules;
 use Illuminate\Http\Request;
 
 class ClientsController extends Controller
@@ -34,13 +35,24 @@ class ClientsController extends Controller
 
     public function store(Request $request)
     {
+        $validated = $request->validate([
+            'name' => ['required', 'max:190'],
+            // We use the `filter` variant of the email rule, because the default implementation
+            // does not check for dot in the domain part.
+            'email' => ['required', 'email:filter'],
+            'phone' => new Rules\PhoneNumber(),
+            'address' => 'nullable',
+            'city' => 'nullable',
+            'postcode' => 'nullable',
+        ]);
+
         $client = new Client;
-        $client->name = $request->get('name');
-        $client->email = $request->get('email');
-        $client->phone = $request->get('phone');
-        $client->address = $request->get('address');
-        $client->city = $request->get('city');
-        $client->postcode = $request->get('postcode');
+        $client->name = $validated['name'];
+        $client->email = $validated['email'];
+        $client->phone = $validated['phone'];
+        $client->address = $validated['address'];
+        $client->city = $validated['city'];
+        $client->postcode = $validated['postcode'];
         $client->save();
 
         return $client;
